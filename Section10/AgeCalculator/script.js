5f2d3f7737758171a114c30944655e9fb133aaf4
Alan Medel <medelalan23@gmail.com>
2020-07-14 07:28:30 -0700

    class Ex section 10

---
+++ Section10/AgeCalculator/script.js
@@ -0,0 +1,4 @@
+let age = prompt("How old are you?");
+let calc = age * 365 + (" days.");
+
+alert(age + " years old is how long you've been alive for roughly " + calc);
---
+++ Section10/ClassEx01/stalkerEx.js
@@ -0,0 +1,7 @@
+var firstName = prompt("Hello what is your first name?");
+var lastName = prompt("What is your last name?");
+var age = prompt("what is your age?")
+var fullName = firstName + " " +lastName
+
+console.log("hello " + fullName + " I am robo stalker");
+console.log("Your age is..." + age);
